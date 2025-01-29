7af0fa0915d19c98cbc900931f67b3e66625c9b6
kabaka <iankabaka1@gmail.com>
2025-01-29 20:00:10 +0300

    formik validations

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,6 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-      </Routes>
-      <Routes>
         <Route path='login' element={<Login />} />
       </Routes>
     </Router>
