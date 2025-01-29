e951536e88926800f0f82b398ac75b8c719b8696
kabaka <iankabaka1@gmail.com>
2025-01-29 20:26:38 +0300

    adds validation and logs values to the console using yup and formik

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -1,8 +1,24 @@
 import {Formik,Form,Field} from 'formik';
+import * as Yup from 'yup';
 function Login(){
     return (
-    <Formik initialValues={{username:'',password:''}}>
-        <Form>
+    <Formik 
+    initialValues={{username:'',password:''}}
+    validationSchema={Yup.object({
+        username: Yup.string()
+            .min(3, 'Username must be at least 3 characters')
+            .required('Username is required'),
+        password: Yup.string()
+            .min(6, 'Password must be at least 6 characters')
+            .required('Password is required'),
+    })}
+    onSubmit={(values) => {
+        console.log('Form submitted with:', values);
+    }}
+    
+    >
+        {({ handleSubmit }) => (
+        <Form onSubmit={handleSubmit}>
             <label>Username:</label>
             <Field type="text" name="username"/>
 
@@ -11,6 +27,7 @@ function Login(){
 
             <button type="submit">Login</button>
         </Form>
+        )}
     </Formik>
     )
 }
