3aa4db78a55ec20e8b551d1cc5a54e47cf557d9e
kabaka <iankabaka1@gmail.com>
2025-01-29 14:00:18 +0300

    adds routing to Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
 import './App.css'
 import Login from './components/Authentication/Login'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
-    <>
-    <Login />
-    </>
+    <Router>
+      <Routes>
+        <Route path='login' element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
