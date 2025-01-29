ffacaf995896dfca55493b18cd36c181a691b592
kabaka <iankabaka1@gmail.com>
2025-01-29 20:43:18 +0300

    sending a POST request to the server

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -12,8 +12,20 @@ function Login(){
             .min(6, 'Password must be at least 6 characters')
             .required('Password is required'),
     })}
-    onSubmit={(values) => {
+    onSubmit={(values, {setSubmitting}) => {
         console.log('Form submitted with:', values);
+
+        fetch('http://127.0.0.1:5000/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(values),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('backend server: ',data )
+        })
+        .catch(error => console.log('Error:', error))
+        .finally(()=> setSubmitting(false))
     }}
     
     >
