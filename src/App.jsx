4551f7c179a5e3f44e298cb4ddf7176888be7ddb
kabaka <iankabaka1@gmail.com>
2025-01-29 14:10:40 +0300

    adds routing to Home

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@
 import './App.css'
 import Login from './components/Authentication/Login'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
 
 
   return (
     <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
       <Routes>
         <Route path='login' element={<Login />} />
       </Routes>
