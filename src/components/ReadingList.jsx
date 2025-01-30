f0a7dd3e8a11c6d64dfeccdf1f1fb9b1d332f92f
kabaka <iankabaka1@gmail.com>
2025-01-31 00:14:23 +0300

    added setter functions in ReadingList component, included dependency arrays in our useeffect and fetched reading lists from the backend

---
+++ src/components/ReadingList.jsx
@@ -0,0 +1,29 @@
+import {userContext} from '../context/userContext';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {Card, CardContent, Typography, Button,TextField, Dialog,DialogActions,DialogContent, DialogTitle} from '@mui/material';
+
+const ReadingList = () => {
+    const {user,isAuthenticated, loading } = userContext();
+    const navigate = useNavigate();
+    const [readingList,setReadingLists] = useState([]);
+    const [isFetching, setIsFetching] = useState(true);
+    const [newListName, setNewListName] = useState("");
+    const [isCreateDialogOpen, setIsCreateDialogOpen] = useState(false);
+    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+    const [currentList, setCurrentList] = useState(null);
+
+    useEffect(() => {
+        if (!loading && !isAuthenticated) {
+            navigate("/login");
+        } else {
+            fetchReadingLists();
+        }
+    },[isAuthenticated,loading,navigate]);
+
+    const fetchReadingLists = () => {
+        fetch("http://127.0.0.1:5000/reading-lists", {
+            credentials: "include",
+
+
+}
