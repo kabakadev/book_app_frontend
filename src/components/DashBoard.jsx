1819acf0eb3ef617fb4b96e61e55de6fb9cd4571
kabaka <iankabaka1@gmail.com>
2025-01-30 10:45:14 +0300

    setting setter functions and context provider

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -2,3 +2,11 @@ import { useUser } from "../context/UserContext.js";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import ReviewsList from "./ReviewsList.jsx";
+
+function DashBoard (){
+    const { user, isAuthenticated, loading } = useUser();
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState(null);
+    const [isFetching, setIsFetching] = useState(true);
+
+}
