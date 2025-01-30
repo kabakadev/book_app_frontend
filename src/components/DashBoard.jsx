76fb05cd44da6460d5dc36fe9aacd6bd0d2b3152
kabaka <iankabaka1@gmail.com>
2025-01-30 10:36:00 +0300

    import ReviewsList component into Dashboard.jsx

---
+++ src/components/DashBoard.jsx
@@ -0,0 +1,4 @@
+import { useUser } from "../context/UserContext.js";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import ReviewsList from "./ReviewsList.jsx";
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,12 @@
 export async function fetchReviews(){
-    
+    try{
+        const response = await fetch("http://localhost:5000/reviews");
+        if (!reposne.ok){
+            throw new Error("Failed to fetch reviews");
+        }
+        return await response.json();
+    }catch (error) {
+        console.error("Error fetching reviews:", error);
+        return [];
+    }
 }
