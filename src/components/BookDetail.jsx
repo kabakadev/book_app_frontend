3f548f61a37564f67f258674cf560b2dc22eb771
kabaka <iankabaka1@gmail.com>
2025-01-31 13:21:02 +0300

    fetch a book by its id and use the setter functions to update the incomeing data from the backend

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -1,3 +1,26 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Typography, Button, TextField, Grid } from '@mui/material';
+
+const BookDetail = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [book,setBook] = useState(null);
+    const [review,setReviews] = useState("");
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:5000/books/${id}`, {
+            credentials: "include",
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setBook(data);
+            setReviews(data.reviews || []);
+        })
+        .catch((error) => {
+            console.error("Error fetching book details:", error);
+        });
+    },[id]);
+    
+}
